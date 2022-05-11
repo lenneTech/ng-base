f9de5230073bb68013a7e3f5c25d74bcba962c67
Pascal Klesse <pascal.klesse@lenne.tech>
2022-05-11 09:11:36 +0200

    fix: Add missing export

--- projects/ng-base/base-prototype/public-api.ts
+++ projects/ng-base/base-prototype/public-api.ts
@@ -8,3 +8,4 @@ export * from './base-prototype.module';
 // Pages
 export * from './pages/login/login.component';
 export * from './pages/register/register.component';
+export * from './pages/forgot-password/forgot-password.component';
