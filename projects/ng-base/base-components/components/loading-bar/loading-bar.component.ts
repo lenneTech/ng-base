5bd80d6e3fd19e4df488aee94ef052b9585fefe1
Pascal Klesse <pascal.klesse@lenne.tech>
2022-07-25 10:42:13 +0200

    fix: Fix loading-bar ExpressionChangedAfterItHasBeenCheckedError

--- projects/ng-base/base-components/components/loading-bar/loading-bar.component.ts
+++ projects/ng-base/base-components/components/loading-bar/loading-bar.component.ts
@@ -1,8 +1,35 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, NgZone, OnDestroy, OnInit } from '@angular/core';
+import { LoaderService } from '@lenne.tech/ng-base/shared';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'base-loading-bar',
   templateUrl: './loading-bar.component.html',
   styleUrls: ['./loading-bar.component.scss'],
 })
-export class LoadingBarComponent {}
+export class LoadingBarComponent implements OnInit, OnDestroy {
+  @Input() manuallyLoading = false;
+  loading = false;
+  subscription = new Subscription();
+
+  constructor(
+    private ngZone: NgZone,
+    private changeDetectorRef: ChangeDetectorRef,
+    private loaderService: LoaderService
+  ) {}
+
+  ngOnInit() {
+    this.ngZone.runOutsideAngular(() => {
+      this.subscription.add(
+        this.loaderService.isLoading.subscribe((value) => {
+          this.loading = value;
+          this.changeDetectorRef.detectChanges();
+        })
+      );
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+}
