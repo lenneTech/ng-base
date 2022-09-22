6a70bc4a7f3b02d1876ac518b1c91c4d308046dc
Florian Steinhanses <florian.steinhanses@lenne.tech>
2022-09-22 18:24:24 +0200

    fixed profile passwords

--- projects/ng-base/shared/validators/secure-password.validator.ts
+++ projects/ng-base/shared/validators/secure-password.validator.ts
@@ -2,7 +2,7 @@ import { AbstractControl, ValidatorFn } from '@angular/forms';
 
 export function securePasswordValidator(): ValidatorFn {
   return (control: AbstractControl) => {
-    const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/i;
+    const PASSWORD_REGEX = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/i;
     return !PASSWORD_REGEX.test(control.value) ? { invalidSecurePassword: true } : null;
   };
 }
