552d81bf331cb914dc53e76f32134089eef1de3d
Pascal Klesse <pascal.klesse@lenne.tech>
2022-10-07 00:47:09 +0200

    feat: Add samples option to find args interface

--- projects/ng-base/shared/interfaces/find-args.interface.ts
+++ projects/ng-base/shared/interfaces/find-args.interface.ts
@@ -2,10 +2,11 @@ import { FilterInput } from './filter-input.interface';
 import { SortInput } from './sort-input.interface';
 
 export interface FindArgs {
-  limit: number;
-  offset: number;
-  skip: number;
-  sort: SortInput[];
-  take: number;
-  filter: FilterInput;
+  limit?: number;
+  offset?: number;
+  skip?: number;
+  sort?: SortInput[];
+  take?: number;
+  samples?: number;
+  filter?: FilterInput;
 }
