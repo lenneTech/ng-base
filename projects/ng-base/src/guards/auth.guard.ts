69e6bc922e91cf6607ac87ce8102bc5c863f4407
Pascal Klesse <pascal.klesse@lenne.tech>
2022-12-06 09:11:02 +0100

    fix: Add different redirect urls to auth guard

--- projects/ng-base/src/guards/auth.guard.ts
+++ projects/ng-base/src/guards/auth.guard.ts
@@ -23,13 +23,25 @@ export class AuthGuard implements CanActivate, CanActivateChild {
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
     return new Observable((subscriber) => {
       let allowed = false;
+      let redirected = false;
+      /* If the user is logged in and the route is not marked as opposite, then the user is allowed to access the route. */
       const opposite = route?.data?.opposite ? route?.data?.opposite : false;
-      const redirectUrl = route?.data?.redirectUrl ? route.data.redirectUrl : this.moduleConfig.authGuardRedirectUrl;
+      const redirectAuthUrl = route?.data?.redirectAuthUrl
+        ? route.data.redirectAuthUrl
+        : this.moduleConfig.authGuardRedirectUrl;
+      const redirectRoleUrl = route?.data?.redirectRoleUrl
+        ? route.data.redirectRoleUrl
+        : this.moduleConfig.authGuardRedirectUrl;
 
       if (this.authService.token && !opposite) {
         if (route.data?.roles) {
           const user = this.authService.currentUser;
           allowed = !!route.data.roles.find((role) => user.roles?.includes(role));
+
+          if (!allowed && redirectRoleUrl) {
+            this.router.navigate([redirectRoleUrl]);
+            redirected = true;
+          }
         } else {
           allowed = true;
         }
@@ -37,8 +49,8 @@ export class AuthGuard implements CanActivate, CanActivateChild {
         allowed = true;
       }
 
-      if (!allowed) {
-        this.router.navigate([redirectUrl]);
+      if (!allowed && redirectAuthUrl && !redirected) {
+        this.router.navigate([redirectAuthUrl]);
       }
 
       subscriber.next(allowed);
