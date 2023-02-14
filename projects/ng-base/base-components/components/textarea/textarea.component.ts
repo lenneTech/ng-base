4ac39212d54eb4188845eccec266920a7bf4138d
Pascal Klesse <pascal.klesse@lenne.tech>
2023-02-14 15:45:27 +0100

    feat: Add auto height option to textarea component

--- projects/ng-base/base-components/components/textarea/textarea.component.ts
+++ projects/ng-base/base-components/components/textarea/textarea.component.ts
@@ -1,11 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'base-textarea',
   templateUrl: './textarea.component.html',
   styleUrls: ['./textarea.component.scss'],
 })
-export class TextareaComponent {
+export class TextareaComponent implements OnChanges {
+  @ViewChild('textarea') textarea: ElementRef;
   @Input() id: string;
   @Input() name: string;
   @Input() infoText: string;
@@ -14,6 +15,37 @@ export class TextareaComponent {
   @Input() control: any;
   @Input() rows = 5;
   @Input() required = false;
+  @Input() autoHeight = true;
   @Input() tabIndex?: number;
   @Input() objectPath = '';
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['control'] && this.autoHeight) {
+      setTimeout(() => {
+        this.adjustInitHeight();
+      }, 350);
+    }
+  }
+
+  adjustInitHeight() {
+    if (!this.textarea || !this.autoHeight) {
+      return;
+    }
+
+    this.textarea.nativeElement.style.cssText = 'height:auto;';
+    this.textarea.nativeElement.style.cssText = 'height:' + (this.textarea.nativeElement.scrollHeight + 5) + 'px';
+  }
+
+  adjustHeight(event) {
+    if (!this.autoHeight) {
+      return;
+    }
+
+    setTimeout(() => {
+      event.target.style.cssText = 'height:auto;';
+      // for box-sizing other than "content-box" use:
+      // el.style.cssText = '-moz-box-sizing:content-box';
+      event.target.style.cssText = 'height:' + (event.target.scrollHeight + 5) + 'px';
+    }, 0);
+  }
 }
