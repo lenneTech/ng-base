23b238580a05bf84e9edb6293c3c9d164544762b
Daniel König <daniel.koenig@lenne.tech>
2023-08-11 12:23:40 +0200

    fix: added isPlatformBrowser to authguard to fix issues when reloading a guarded route

--- projects/ng-base/src/guards/auth.guard.ts
+++ projects/ng-base/src/guards/auth.guard.ts
@@ -1,7 +1,8 @@
-import { Inject, Injectable } from '@angular/core';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService, BASE_MODULE_CONFIG, BaseModuleConfig } from '@lenne.tech/ng-base/shared';
+import { isPlatformBrowser } from '@angular/common';
 
 /**
  * Authentication guard
@@ -14,56 +15,59 @@ export class AuthGuard implements CanActivate, CanActivateChild {
   constructor(
     private router: Router,
     private authService: AuthService,
-    @Inject(BASE_MODULE_CONFIG) private moduleConfig: BaseModuleConfig
+    @Inject(BASE_MODULE_CONFIG) private moduleConfig: BaseModuleConfig,
+    @Inject(PLATFORM_ID) private platformId: object
   ) {}
 
   /**
    * Can activate main route
    */
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-    return new Observable((subscriber) => {
-      let allowed = false;
-      let redirected = false;
-      /* If the user is logged in and the route is not marked as opposite, then the user is allowed to access the route. */
-      const opposite = route?.data?.opposite ? route?.data?.opposite : false;
-      const redirectAuthUrl = route?.data?.redirectAuthUrl
-        ? route.data.redirectAuthUrl
-        : this.moduleConfig.authGuardRedirectUrl;
-      const redirectRoleUrl = route?.data?.redirectRoleUrl
-        ? route.data.redirectRoleUrl
-        : this.moduleConfig.authGuardRedirectUrl;
-      const disableQueryParams = route?.data?.disableQueryParams ?? false;
-      const queryParams = disableQueryParams
-        ? {}
-        : {
-            queryParams: {
-              redirectUrl: route['_routerState'].url,
-            },
-          };
+    if (isPlatformBrowser(this.platformId)) {
+      return new Observable((subscriber) => {
+        let allowed = false;
+        let redirected = false;
+        /* If the user is logged in and the route is not marked as opposite, then the user is allowed to access the route. */
+        const opposite = route?.data?.opposite ? route?.data?.opposite : false;
+        const redirectAuthUrl = route?.data?.redirectAuthUrl
+          ? route.data.redirectAuthUrl
+          : this.moduleConfig.authGuardRedirectUrl;
+        const redirectRoleUrl = route?.data?.redirectRoleUrl
+          ? route.data.redirectRoleUrl
+          : this.moduleConfig.authGuardRedirectUrl;
+        const disableQueryParams = route?.data?.disableQueryParams ?? false;
+        const queryParams = disableQueryParams
+          ? {}
+          : {
+              queryParams: {
+                redirectUrl: route['_routerState'].url,
+              },
+            };
 
-      if (this.authService.token && !opposite) {
-        if (route.data?.roles) {
-          const user = this.authService.currentUser;
-          allowed = !!route.data.roles.find((role) => user.roles?.includes(role));
+        if (this.authService.token && !opposite) {
+          if (route.data?.roles) {
+            const user = this.authService.currentUser;
+            allowed = !!route.data.roles.find((role) => user.roles?.includes(role));
 
-          if (!allowed && redirectRoleUrl) {
-            this.router.navigate([redirectRoleUrl], queryParams);
-            redirected = true;
+            if (!allowed && redirectRoleUrl) {
+              this.router.navigate([redirectRoleUrl], queryParams);
+              redirected = true;
+            }
+          } else {
+            allowed = true;
           }
-        } else {
+        } else if (!this.authService.token && opposite) {
           allowed = true;
         }
-      } else if (!this.authService.token && opposite) {
-        allowed = true;
-      }
 
-      if (!allowed && redirectAuthUrl && !redirected) {
-        this.router.navigate([redirectAuthUrl], queryParams);
-      }
+        if (!allowed && redirectAuthUrl && !redirected) {
+          this.router.navigate([redirectAuthUrl], queryParams);
+        }
 
-      subscriber.next(allowed);
-      subscriber.complete();
-    });
+        subscriber.next(allowed);
+        subscriber.complete();
+      });
+    }
   }
 
   /**
